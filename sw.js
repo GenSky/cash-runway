78c7448ee9f6f68a7f5e255b5e9191d16abc04e4
attilajax <29130088+GenSky@users.noreply.github.com>
2026-06-09 21:15:52 -0700

    Improve navigation and table scrolling UX

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cash-runway-v10";
+const CACHE_NAME = "cash-runway-v11";
 const APP_SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
